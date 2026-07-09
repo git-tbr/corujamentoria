a322c02f789c2630b01298d84fab1ea53285b0e4
Willian Eliseu da Silva <w.eliseu@gmail.com>
2026-07-09 17:25:00 -0300

    site atualizado para publicação

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import { RouterView, useRouter } from 'vue-router'
+import { RouterView } from 'vue-router'
 import { onMounted } from 'vue'
 import GlobalDialog from './components/GlobalDialog.vue'
 
-const router = useRouter();
+// const router = useRouter();
 
-const getIpAdress = async () => {
-  const response = await fetch('https://api.ipify.org?format=json');
-  const data = await response.json();
-  return data.ip;
-}//remover após a atualização
+// const getIpAdress = async () => {
+//   const response = await fetch('https://api.ipify.org?format=json');
+//   const data = await response.json();
+//   return data.ip;
+// }//remover após a atualização
 
 onMounted(() => {
-  getIpAdress().then((ip) => {
-    if (ip !== '191.13.130.133') {
-      router.push({ name: 'manutencao' })
-    }
-  });//remover após a atualização
+  // getIpAdress().then((ip) => {
+  //   if (ip !== '191.13.130.133') {
+  //     router.push({ name: 'manutencao' })
+  //   }
+  // });//remover após a atualização
 
   setTimeout(() => {
     window.scrollTo(0, 0);
